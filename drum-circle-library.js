5313da97784b4f8a1cce8dbec8caffcca60d2966
Ken Fehling <ken@androidideas.org>
2014-09-14 01:30:22 -0400

    Try this

--- drum-circle-library.js
+++ drum-circle-library.js
@@ -11,11 +11,14 @@ if (typeof define !== 'function') {
     var define = require('amdefine')(module);
 }
 
-define(['time_utils', 'utils', 'constants'], function(time_utils, utils, constants) {
-    "use strict";
-    return {
-        time_utils: time_utils,
-        utils: utils,
-        constants: constants
-    };
-});
+define(['drum-circle-library/time_utils',
+        'drum-circle-library/utils',
+        'drum-circle-library/constants'],
+    function(time_utils, utils, constants) {
+        "use strict";
+        return {
+            time_utils: time_utils,
+            utils: utils,
+            constants: constants
+        };
+    });
